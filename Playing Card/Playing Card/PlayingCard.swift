fc50ed5255d249a014a1a44766a15e2c520e87cd
Martín4691 <martinsm1991@gmail.com>
2021-04-11 16:37:25 +0200

    Today last update (Lecture 5 CS193p)

--- Playing Card/Playing Card/PlayingCard.swift
+++ Playing Card/Playing Card/PlayingCard.swift
@@ -8,12 +8,16 @@
 import Foundation
 
 
-struct PlayingCard {
+struct PlayingCard: CustomStringConvertible {
+    
+    var description: String { return "\(rank)\(suit)" }
     
     var suit: Suit
     var rank: Rank
     
-    enum Suit: String {
+    enum Suit: String, CustomStringConvertible {
+        var description: String
+        
         case spades = "♠︎"
         case hearts = "♥︎"
         case diamonds = "♦︎"
@@ -22,7 +26,9 @@ struct PlayingCard {
         static var all = [Suit.spades, .hearts, .diamonds, .clubs]
     }
     
-    enum Rank {
+    enum Rank: CustomStringConvertible {
+        var description: String
+        
         case ace
         case face(String)
         case numeric(Int)
@@ -30,7 +36,7 @@ struct PlayingCard {
         var order: Int {
             switch self {
             case .ace: return 1
-            case .numeric
+            case .numeric(let pips): return pips
             case .face(let kind) where kind == "J": return 11
             case .face(let kind) where kind == "Q": return 12
             case .face(let kind) where kind == "K": return 13
